7dbc7c62a67ec8e47398210922e1a2d4481b5c61
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-10-13 16:06:42 +0545

    getServerSideProps getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  console.log(events)
   return (
     <Layout>
       <h1>Home</h1>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+  }
 }
