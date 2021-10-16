81aacf7ebbf639c4a4e3846b74c19d1a818c3d0a
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-10-16 20:40:47 +0545

    modal component

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.css'
 
@@ -24,6 +25,7 @@ export default function EditEventPage({ evt }) {
   const [imagePreview, setImagePreview] = useState(
     evt.image ? evt.image.formats.thumbnail.url : null
   )
+  const [showModal, setShowModal] = useState(false)
 
   const router = useRouter()
 
@@ -160,6 +162,10 @@ export default function EditEventPage({ evt }) {
           <FaImage /> Set Image
         </button>
       </div>
+
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        IMAGE UPLOAD
+      </Modal>
     </Layout>
   )
 }
